bc449b59c755ff1a92412c0ee88cf553690b49ca
abhijeetk <abhijeetk@DESKTOP-GBJIH7F>
2022-09-06 20:42:52 -0800

    Add project files.

---
+++ OrderDemo/Entities/OrderDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderDemo.Entities
+{
+    public class OrderDetails
+    {
+        public int detailsId { get; set; }
+        public int productId { get; set; }
+        public string? productName { get; set; }
+        public double rate { get; set; }
+        public int quentity { get; set; }
+        public double totalAmount { get; set; }
+        public int orderId { get; set; }
+    }
+}
---
+++ OrderDemo/Repositories/Interface/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using OrderDemo.Entities;
+
+namespace OrderDemo.Repositories.Interface
+{
+    public interface IOrderRepository
+    {
+        public Task<IEnumerable<Order>> GetOrders();
+        public Task<Order> GetOrderById(int id);
+        public Task<int> PlaceOrder(Order order);
+        public Task<int> UpdateOrder(Order order);
+
+        public Task<int> Delete(int id);
+    }
+}
